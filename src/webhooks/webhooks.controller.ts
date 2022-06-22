05b061a77b35e7d50bf4d7138dbde27988676c17
Sotatek-DongBui <dong.bui@sotatek.com>
2022-06-22 08:44:33 +0700

    change pull rrequest

--- src/webhooks/webhooks.controller.ts
+++ src/webhooks/webhooks.controller.ts
@@ -5,7 +5,7 @@ import { WebhooksService } from './webhooks.service';
 @Controller('webhooks')
 export class WebhooksController {
   constructor(private readonly webhooksService: WebhooksService) {}
-  @Post('github')
+  @Post('github/pull-request')
   async notiGithubWebhook(@Body() payload: IPayloadGithub) {
     await this.webhooksService.notiGithubWebhook(payload);
     return 'ok';
