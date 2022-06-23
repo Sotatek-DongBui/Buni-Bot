3885aeac8752cbed6aac11d79e03a5cc34b8998b
Sotatek-DongBui <dong.bui@sotatek.com>
2022-06-23 15:13:34 +0700

    rename port env

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,8 +1,8 @@
-const { GOOGLE_CHAT_BOT, PORT, HOST } = process.env;
+const { GOOGLE_CHAT_BOT, PORT_BOT, HOST } = process.env;
 
 export const configs = {
   googleChatBotUrl: GOOGLE_CHAT_BOT,
-  port: PORT,
+  port: PORT_BOT,
   globalPath: '/buni-bot',
   currentPath: process.cwd(),
   host: HOST,
